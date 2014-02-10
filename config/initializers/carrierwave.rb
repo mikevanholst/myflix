b44572aac004068f2a8015d6380a3512d1806426
Michael van Holst <mikevanholst@gmail.com>
2014-02-09 23:35:27 -0500

    fixes file upload in dev

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,15 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                      # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
-  }
-  config.fog_directory  = 'name_of_directory'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  if Rails.env.staging? || Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',                        # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                      # required
+    }
+    config.fog_directory  = 'mvh-myflix'                     # required
+  else
+    config.storage = :file
+  end
 end
+  
 
