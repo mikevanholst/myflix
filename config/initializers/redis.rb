3e5441c66dee3288e6255f4320fb0a5f0cb59495
Michael van Holst <mikevanholst@gmail.com>
2014-02-14 11:14:49 -0500

    apply redis settings only to developemnt

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,12 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"])
-# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-ENV["REDISTOGO_URL"] = 'redis://localhost:6379/0'
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# a little confusion with this file
 
+if RAils.env.development?
+  # uri = URI.parse(ENV["REDISTOGO_URL"])
+  # REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  ENV["REDISTOGO_URL"] = 'redis://localhost:6379/0'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
+end
 
 # uri = URI.parse(URI.encode(url.strip))
