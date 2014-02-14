1d98b87b99cee328e7a8f37ed84da9ca44359122
Michael van Holst <mikevanholst@gmail.com>
2014-02-14 11:22:58 -0500

    corrects typo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 # a little confusion with this file
 
-if RAils.env.development?
+if Rails.env.development?
   # uri = URI.parse(ENV["REDISTOGO_URL"])
   # REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   ENV["REDISTOGO_URL"] = 'redis://localhost:6379/0'
