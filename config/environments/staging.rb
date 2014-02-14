eac69be40855c79e8b5b4a97d94dc39b2b1098ce
Michael van Holst <mikevanholst@gmail.com>
2014-02-14 09:57:58 -0500

    adds staging envrionment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,7 +2,6 @@ Myflix::Application.configure do
 
   config.cache_classes = true
   config.eager_load = true
-  # config.consider_all_requests_local       = false
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
