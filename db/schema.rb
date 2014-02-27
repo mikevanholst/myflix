c564c26ef89933de9d24dca62fd58c5e89a5c0c5
Michael van Holst <mikevanholst@gmail.com>
2014-02-27 18:12:46 -0500

    first webhook test passes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213165651) do
+ActiveRecord::Schema.define(version: 20140227180342) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20140213165651) do
     t.datetime "updated_at"
     t.boolean  "active"
     t.string   "token"
+  end
+
+  create_table "payments", force: true do |t|
+    t.integer "user_id"
+    t.integer "amount"
+    t.string  "reference_id"
   end
 
   create_table "queue_items", force: true do |t|
--- spec/requests/test_stripe_webhooks_spec.rb
+++ spec/requests/test_stripe_webhooks_spec.rb
@@ -1,55 +1,66 @@
 require 'spec_helper'
 
 describe "test stripe webhooks" do
-  let(:event_data) do
-    {
-      id: "ch_103ZNM2osGI26Z41JD0jWOku", #was variable
-      object: "charge",
-      created: 1393447706,
-      livemode: false,
-      paid: true,
-      amount: 999,
-      currency: "cad",
-      refunded: false,
-      card:
-      {
-        id: "card_103ZNM2osGI26Z41NTvSGZ7z", #was variable
-        object: "card",
-        last4: "4242",
-        type: "Visa",
-        exp_month: 2,
-        exp_year: 2014,
-        fingerprint: "yYUTBmoos0c1k1j3",
-        customer: "cus_3ZNMjdxHXqmu4C", #was variable
-        country: "US",
-        name: nil,
-        address_line1: nil,
-        address_line2: nil,
-        address_city: nil,
-        address_state: nil,
-        address_zip: nil,
-        address_country: nil,
-        cvc_check: "pass",
-        address_line1_check: nil,
-        address_zip_check: nil,
+  let!(:event_data) do
+   {
+      "id" =>  "evt_103Zkm2osGI26Z416yTR2hsR",
+      "created" =>  1393534877,
+      "livemode" =>  false,
+      "type" =>  "charge.succeeded",
+      "data" =>  {
+        "object" =>  {
+          "id" =>  "ch_103Zkm2osGI26Z41KnyHA7LC",
+          "object" =>  "charge",
+          "created" =>  1393534877,
+          "livemode" =>  false,
+          "paid" =>  true,
+          "amount" =>  999,
+          "currency" =>  "cad",
+          "refunded" =>  false,
+          "card" =>  {
+            "id" =>  "card_103Zkm2osGI26Z410ETXp26M",
+            "object" =>  "card",
+            "last4" =>  "4242",
+            "type" =>  "Visa",
+            "exp_month" =>  2,
+            "exp_year" =>  2014,
+            "fingerprint" =>  "yYUTBmoos0c1k1j3",
+            "customer" =>  "cus_3Zkm0FSpuJqJFf",
+            "country" =>  "US",
+            "name" =>  nil,
+            "address_line1" =>  nil,
+            "address_line2" =>  nil,
+            "address_city" =>  nil,
+            "address_state" =>  nil,
+            "address_zip" =>  nil,
+            "address_country" =>  nil,
+            "cvc_check" =>  "pass",
+            "address_line1_check" =>  nil,
+            "address_zip_check" =>  nil
+          },
+          "captured" =>  true,
+          "refunds" =>  [],
+          "balance_transaction" => "txn_103Zkm2osGI26Z41rxYEJnT7",
+          "failure_message" =>  nil,
+          "failure_code" =>  nil,
+          "amount_refunded" =>  0,
+          "customer" =>  "cus_3Zkm0FSpuJqJFf",
+          "invoice" =>  "in_103Zkm2osGI26Z41G9VU047A",
+          "description" =>  nil,
+          "dispute" =>  nil,
+          "metadata" =>  {}
+        }
       },
-
-      captured: true,
-      refunds:  {},
-      balance_transaction: "txn_103ZNM2osGI26Z41IL390k5D",
-      failure_message: nil,
-      failure_code: nil,
-      amount_refunded: 0,
-      customer: "cus_3ZNMjdxHXqmu4C",  #was variable
-      invoice: "in_103ZNM2osGI26Z41S6UhDDpV",  #was variable
-      description: nil,
-      dispute: nil,
-      metadata:  
-      {}
-    }
+      "object" =>  "event",
+      "pending_webhooks" =>  1,
+      "request" =>  "iar_3ZkmiQejAXDCDh"
+      }
   end
   it "it creates a payment with a stripe webhook on a successful charge", :vcr do
     post "/stripe_events", event_data
     expect(Payment.count).to eq(1)
+    
+
   end
 end
+
